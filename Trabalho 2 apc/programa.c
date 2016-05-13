e91ce7bfe248788443b89b874f9c51b161ffc97d
Douglas Vieira Alcantra <T1073801@DF.INTRABB.BB.COM.BR>
2016-05-13 15:59:43 -0300

    2

--- Trabalho 2 apc/programa.c
+++ Trabalho 2 apc/programa.c
@@ -108,20 +108,72 @@ void ler_string(char string_1[], char string_2[]){
 }
 int encoontra_sub_string(char string_fim[],char sub_string[])
 {
-	
-
+	// int tamanho=0;
+	// for (tamanho = 0 ; sub_string[i]!='\n' && sub_string[i] != '\0' && sub_string[i]!='\r'; tamanho++)
+	// {}
+	return(strstr(string_fim,sub_string)?1:0)
 }
 int main ()
 {
+	int genes[100]={0,0,0,0,0,0,0,0,0,0};
+	char gen[23][11]; 
 	char string_1[100], string_2[100], string_fim[100];
 	memset(string_fim,'\0',100*sizeof(char));
-	ler_string (string_1,string_2);
+	memset(gen,'\0',23*11*sizeof(char));
+	
+	strcmp (gen[0], "TCCTGCCATG");
+	strcmp (gen[1], "TGTATGAAGT");
+	strcmp (gen[2], "CTCGAAATTA");
+	strcmp (gen[3], "TTCTACGTAC");
+	strcmp (gen[4], "TTAGATACGG");
+	strcmp (gen[5], "TGAAGGGA");
+	strcmp (gen[6], "AGAGTCATGC");
+	strcmp (gen[7], "CTACGG");
+	strcmp (gen[8], "TGACGGCG");
+	strcmp (gen[9], "GAGGTGGG");
+	strcmp (gen[10], "TTGAGGCA");
+	strcmp (gen[11], "TTGGCGG");
+	strcmp (gen[12], "CACATA");
+	strcmp (gen[13], "AGCCCGGC");
+	strcmp (gen[14], "CTACGG");
+	strcmp (gen[15], "CTGGAGCTGT");
+	strcmp (gen[16], "GAGCGG");
+	strcmp (gen[17], "ACTCAC");
+	strcmp (gen[18], "GAGTTTATAA");
+	strcmp (gen[19], "CATATA");
+	strcmp (gen[20], "TATGACTC");
+	strcmp (gen[21], "TTGATTCG");
+	strcmp (gen[22], "TACATA");
 
+	ler_string (string_1,string_2);
 	printf("string1:\t%s\n",string_1 );
 	printf("string2:\t%s\n",string_2 );
-	
 	concatena (string_1,string_2,string_fim);
 	printf("string fim:\t%s\n",string_fim );
+	genes [0] = encoontra_sub_string (string_fim, "");
+	genes [1] = encoontra_sub_string (string_fim, "");
+	genes [2] = encoontra_sub_string (string_fim, "");
+	genes [3] = encoontra_sub_string (string_fim, "");
+	genes [4] = encoontra_sub_string (string_fim, "");
+	genes [5] = encoontra_sub_string (string_fim, "");
+	genes [6] = encoontra_sub_string (string_fim, "");
+	genes [7] = encoontra_sub_string (string_fim, "");
+	genes [8] = encoontra_sub_string (string_fim, "");
+	genes [9] = encoontra_sub_string (string_fim, "");
+	genes[10] = encoontra_sub_string (string_fim, "");
+
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
 
 	return 0;
 }
